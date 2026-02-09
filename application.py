4ac6f1dfef87a28544be5e15428838a4b75e9d78
jathinreddy3515 <jathin498@gmail.com>
2026-02-09 12:06:19 +0530

    Render Deploy

--- application.py
+++ application.py
@@ -28,8 +28,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),  # ✅ fixed
-            writing_score=float(request.form.get('writing_score'))   # ✅ fixed
+            reading_score=float(request.form.get('reading_score')),  
+            writing_score=float(request.form.get('writing_score'))   
         )
 
         # Convert input to DataFrame for prediction
@@ -52,4 +52,4 @@ def predict_datapoint():
 # Run Flask App
 # -------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=10000)
